d292e987596644df066e5641b777afedfe05c195
KrishnaManaswiD <dkrishnamanaswi@gmail.com>
2021-04-24 21:49:50 +0200

    Added infinite road and asteroid spawning

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -10,26 +10,36 @@ public class GameController : MonoBehaviour
 
 
     // variables specific to this script
-    public Transform road;
+    public GameObject road;
     private float roadSpawnPosition = 0;
 
-    public Transform asteroid;
+    public GameObject asteroid;
     private float asteroidPosition = 0.0f;
 
-    public Transform boost;
+    public GameObject boost;
 
-    public Transform player;
+    public GameObject player;
+    private Transform playerTransform;
     private Vector3 playerStartingPosition = new Vector3(0, 1.01f, 0);
 
+    private int numberOfRoadPiecesInfrontOfPlayer = 25;
+    
 
     // Start is called before the first frame update
     void Start()
     {
         // spawn player
-        Instantiate(player, playerStartingPosition, player.rotation);
+        Instantiate(player);
+        player.transform.position = playerStartingPosition;
 
+        // create initial road
+        for (int i = 0; i < numberOfRoadPiecesInfrontOfPlayer; i++)
+        {
+            SpawnRoad();
+            SpawnAsteroid();
+        }
 
-        Instantiate(boost, new Vector3(0, 0.7f, 10.0f), boost.rotation);
+        SpawnBoost();
         
     }
 
@@ -37,23 +47,43 @@ public class GameController : MonoBehaviour
     void Update()
     {
         // create road
-        if (roadSpawnPosition < 50)
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        Debug.Log(playerTransform.position.z);
+        if (playerTransform.position.z > (roadSpawnPosition - numberOfRoadPiecesInfrontOfPlayer * 1))
         {
-            // create road
-            Instantiate(road, new Vector3(0, 0, roadSpawnPosition), road.rotation);
-
-            asteroidPosition = Random.Range(-5, 5);
-            // create asteroids
-            if (Mathf.Abs(asteroidPosition % 2.0f) < 0.5 )
-            {
-                Instantiate(asteroid, new Vector3(asteroidPosition, 1.5f, roadSpawnPosition), asteroid.rotation);
-            }
-            
-
-            roadSpawnPosition += 2;
+            Debug.Log("need road");
+            SpawnRoad();
+            SpawnAsteroid();
         }
 
         // update time spent
         timeSpent += Time.deltaTime;
+    }
+
+    private void SpawnRoad()
+    {
+        GameObject newRoad;
+        newRoad = Instantiate(road) as GameObject;
+        newRoad.transform.position = new Vector3(0, 0, roadSpawnPosition);
+        roadSpawnPosition += 2;
+    }
+
+    private void SpawnAsteroid()
+    {
+        asteroidPosition = Random.Range(-5, 5);
+        // create asteroids
+        if (Mathf.Abs(asteroidPosition % 2.0f) < 0.5)
+        {
+            GameObject newAsteroid;
+            newAsteroid = Instantiate(asteroid) as GameObject;
+            newAsteroid.transform.position = new Vector3(asteroidPosition, 1.5f, roadSpawnPosition);
+        }
+    }
+
+    private void SpawnBoost()
+    {
+        GameObject newBoost;
+        newBoost = Instantiate(boost) as GameObject;
+        newBoost.transform.position = new Vector3(0, 0.7f, 10.0f);
     }
 }
