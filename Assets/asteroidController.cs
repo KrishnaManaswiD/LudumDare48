f03079e7b980bc18d085e69cf11003403619d99d
KrishnaManaswiD <dkrishnamanaswi@gmail.com>
2021-04-24 20:55:27 +0200

    added scripts to delet road and asterorids when they go off camera

---
+++ Assets/asteroidController.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class asteroidController : MonoBehaviour
+{
+    // destroy this game object if it goes off camera
+    void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
