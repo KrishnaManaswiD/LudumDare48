a6dbf27d6f1aa7645de7cd5d884d4dc73e85b477
KrishnaManaswiD <dkrishnamanaswi@gmail.com>
2021-04-24 18:18:54 +0200

    player movement and rotation done

--- Assets/surferController.cs
+++ Assets/surferController.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class surferController : MonoBehaviour
 {
+    public float horizontalVelocity = 0;
+    public float verticalVelocity = 0;
+    public float rotationAboutForward = 0;
+    public float rotationAboutVertical = 0;
+    
+    public int  currentLaneNumber = 3; // 1, 2, 3, 4, 5
+
+    public string movementLock = "n";
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +22,37 @@ public class surferController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 4);
+        GetComponent<Rigidbody>().velocity = new Vector3(horizontalVelocity, verticalVelocity, 4);
+        GetComponent<Rigidbody>().angularVelocity = new Vector3(0, rotationAboutVertical, rotationAboutForward);
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && currentLaneNumber > 1 && movementLock == "n")
+        {
+            horizontalVelocity = -3.0f;
+            verticalVelocity = currentLaneNumber > 3 ? -0.5f : 0.5f;
+            rotationAboutForward = -0.5f;
+            StartCoroutine(stopSliding());
+            currentLaneNumber -= 1;
+            movementLock = "y";
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow) && currentLaneNumber < 5 && movementLock == "n")
+        {
+            horizontalVelocity = 3.0f;
+            verticalVelocity = currentLaneNumber < 3 ? -0.5f : 0.5f;
+            rotationAboutForward = 0.5f;
+            StartCoroutine(stopSliding());
+            currentLaneNumber += 1;
+            movementLock = "y";
+        }
+    }
+
+    // stops player movement after a certain amount of time
+    IEnumerator stopSliding()
+    {
+        yield return new WaitForSeconds(0.5f);
+        horizontalVelocity = 0.0f;
+        verticalVelocity = 0.0f;
+        rotationAboutForward = 0;
+        movementLock = "n";
     }
 }
