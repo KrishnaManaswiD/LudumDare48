3482444716160dd5299d096e5a3001d7e019dbac
KrishnaManaswiD <dkrishnamanaswi@gmail.com>
2021-04-25 01:14:06 +0200

    corrected error in camera shake

--- Assets/Scripts/cameraController.cs
+++ Assets/Scripts/cameraController.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class cameraController : MonoBehaviour
 {
     private float forwardVelocity = 20.0f;
-    private float horizontalVelocity = 1.0f;
+    private float horizontalVelocity = 0.0f;
+    private float timeElapsed = 0f;
 
     // Start is called before the first frame update
     void Start()
@@ -20,21 +21,39 @@ public class cameraController : MonoBehaviour
 
         if(GameController.boostMode)
         {
-            horizontalVelocity = Random.Range(-2f, 2f);
-            forwardVelocity = 18.0f;
-            StartCoroutine(StopBoost());
+            horizontalVelocity = Random.Range(-1f, 1f);
+            if (timeElapsed < 1.3f)
+            {
+                forwardVelocity = 18.0f;
+            }
+            else
+            {
+                forwardVelocity = 33.0f;
+            }
+            timeElapsed += Time.deltaTime;
         }
         else
         {
-            horizontalVelocity = 0;
+            timeElapsed = 0f;
             forwardVelocity = 20.0f;
+            horizontalVelocity = 0.0f;
+            // camera doesnt exactly come back to same x
         }
+
+    }
+    /*
+    IEnumerator Recover()
+    {
+        
+        yield return new WaitForSeconds(1.4f);
+        forwardVelocity = 48.0f;
     }
 
     IEnumerator StopBoost()
     {
-        yield return new WaitForSeconds(1.4f);
-        forwardVelocity = 48.0f;
-    }
+        GameController.boostMode = false;
+        yield return new WaitForSeconds(1.5f);
+        forwardVelocity = 20.0f;
+    }*/
 }
 
