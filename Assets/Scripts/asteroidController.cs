a20415cd15847a42a6c56a0878d873b903d2fe96
KrishnaManaswiD <dkrishnamanaswi@gmail.com>
2021-04-25 14:20:59 +0200

    asteroids rotate

--- Assets/Scripts/asteroidController.cs
+++ Assets/Scripts/asteroidController.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class asteroidController : MonoBehaviour
 {
+
+    private void Update()
+    {
+        float randomRotationVelocity = Random.Range(1f, 3f); 
+        GetComponent<Rigidbody>().angularVelocity = new Vector3(randomRotationVelocity, randomRotationVelocity, randomRotationVelocity);
+    }
+
     // destroy this game object if it goes off camera
     void OnBecameInvisible()
     {
